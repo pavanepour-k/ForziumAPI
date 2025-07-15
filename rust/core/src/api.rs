2ea21503d632655e0a037da0f2f1a08c5d7d3dd4
pavanepour-k <pavanepour@outlook.com>
2025-07-15 19:21:58 +0900

    Expose the new modules

--- rust/core/src/api.rs
+++ rust/core/src/api.rs
@@ -1,2 +1,4 @@
-pub use crate::validation::{validate_buffer_size, validate_utf8_string, validate_u8_range};
+pub use crate::dependencies::{Dependency, DependencyResolver, DependencyScope};
+pub use crate::routing::{parse_route_pattern, HttpMethod, Route, RouteMatch, RouteMatcher};
 pub use crate::types::{InputData, OutputData};
+pub use crate::validation::{validate_buffer_size, validate_u8_range, validate_utf8_string};

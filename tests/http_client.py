de5c4d6d6b5af613d8e2a9c7e4abd2b18bb539b0
pavanepour-k <83198025+pavanepour-k@users.noreply.github.com>
2025-10-07 12:31:47 +0900

    Move tests directory

--- tests/http_client.py
+++
@@ -1,5 +0,0 @@
-"""Expose stdlib HTTP helpers for tests."""
-
-from .test_http_client import Response, get, post
-
-__all__ = ["Response", "get", "post"]
--- tests/test_cli_plugins.py
+++
@@ -1,8 +0,0 @@
-"""Tests for CLI plugin discovery."""
-
-from forzium.cli import main
-
-
-def test_plugin_hello(capsys) -> None:
-    main(["hello"])
-    assert "hello plugin" in capsys.readouterr().out

6a4426dd15af321d74842b5be93dcec5ba984d2d
pavanepour-k <pavanepour@outlook.com>
2025-10-09 04:52:44 +0900

    On branch test/feature

--- load_generators/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Utilities and templates for ForziumAPI load generation stacks."""
-
-from __future__ import annotations
-
-__all__ = [
-    "__doc__",
-]
--- plugins/sample_plugin/forzium_sample_plugin/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Sample plugin package used solely for integration testing."""
-
-from __future__ import annotations
-
-__all__ = ("PLUGIN_IDENTIFIER",)
-
-# Exposed constant to prove module import works during tests
-PLUGIN_IDENTIFIER = "forzium.sample-plugin"
